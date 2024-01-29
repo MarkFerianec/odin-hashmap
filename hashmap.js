e0284c0c382d4178a8488e9435178382ce86f18f
Mark Ferianec <134004771+MarkFerianec@users.noreply.github.com>
2024-01-29 10:04:23 -0700

    Create index and hashmap files
    Created files for a hashmap implementation assignment.

---
+++ hashmap.js
@@ -0,0 +1,12 @@
+class HashMap {
+//   defaultBucketsSize = 16;
+//   buckets = Array(this.defaultBucketsSize);
+//   capacity = this.buckets.length;
+//   loadFactor = 0.75;
+}
+
+let myHashMap = new HashMap();
+
+// console.log(myHashMap.buckets);
+// console.log(myHashMap.capacity);
+// console.log(myHashMap.loadFactor);
