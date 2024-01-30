6344d7dd77a6cdcec237944e55ed5b589faa3bda
Mark Ferianec <134004771+MarkFerianec@users.noreply.github.com>
2024-01-30 12:08:21 -0700

    Modify hashmap
    Added a hash function which takes a key and produces a hash code
    with it.

--- hashmap.js
+++ hashmap.js
@@ -1,12 +1,30 @@
 class HashMap {
-//   defaultBucketsSize = 16;
-//   buckets = Array(this.defaultBucketsSize);
-//   capacity = this.buckets.length;
-//   loadFactor = 0.75;
+  // defaultBucketsSize = 16;
+  // buckets = Array(this.defaultBucketsSize);
+  // capacity = this.buckets.length;
+  // loadFactor = 0.75;
+
+  hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+    }
+
+    return hashCode;
+    // return hashCode % this.capacity;
+  }
 }
+
+class Node {}
 
 let myHashMap = new HashMap();
 
 // console.log(myHashMap.buckets);
 // console.log(myHashMap.capacity);
 // console.log(myHashMap.loadFactor);
+
+console.log(myHashMap.hash('Mark'));
+console.log(myHashMap.hash('Bob'));
+console.log(myHashMap.hash('Steven'));
