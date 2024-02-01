e38cb89c1c2ee5d4b9e9aaab360a4a5e1e90bab6
Mark Ferianec <134004771+MarkFerianec@users.noreply.github.com>
2024-02-01 01:42:00 -0700

    Modify hashmap
    Added a basic set function which takes two arguments, the first is a
    key and the second is a value that is assigned to this key.

--- hashmap.js
+++ hashmap.js
@@ -15,6 +15,25 @@ class HashMap {
     // return hashCode;
     return hashCode % this.capacity;
   }
+
+  set(key, value) {
+    // if the buckets have reached loadfactor of 0.75
+    // increase buckets size * 2, copy over all nodes.
+    // not just that, we need to make a linked list with
+    // new nodes added to shared buckets
+
+    if (this.buckets[this.hash(key)] === undefined) {
+      let newNode = new Node(key, value);
+      this.buckets[this.hash(key)] = newNode;
+    } else {
+      let newNode = new Node(key, value);
+      let temporary = this.buckets[this.hash(key)];
+      while (temporary.nextNode != null) {
+        temporary = temporary.nextNode;
+      }
+      temporary.nextNode = newNode;
+    }
+  }
 }
 
 class Node {
@@ -27,10 +46,36 @@ class Node {
 
 let myHashMap = new HashMap();
 
-console.log(myHashMap.buckets);
-console.log(myHashMap.capacity);
-console.log(myHashMap.loadFactor);
+// console.log(myHashMap.buckets);
+// console.log(myHashMap.capacity);
+// console.log(myHashMap.loadFactor);
 
-console.log(myHashMap.hash('Mark'));
-console.log(myHashMap.hash('Bob'));
+// console.log(myHashMap.hash('Mark'));
+// console.log(myHashMap.hash('Bob'));
 console.log(myHashMap.hash('Steven'));
+// console.log(myHashMap.hash('James'));
+// console.log(myHashMap.hash('Patricia'));
+// console.log(myHashMap.hash('Jennifer'));
+console.log(myHashMap.hash('John'));
+// console.log(myHashMap.hash('Elizabeth'));
+// console.log(myHashMap.hash('William'));
+// console.log(myHashMap.hash('Richard'));
+console.log(myHashMap.hash('Sarah'));
+
+// myHashMap.set('Mark', 'Something');
+// myHashMap.set('Bob', 'Something');
+myHashMap.set('Steven', '1');
+// myHashMap.set('James', 'Something');
+// myHashMap.set('Patricia', 'Something');
+// myHashMap.set('Jennifer', 'Something');
+myHashMap.set('John', '2');
+// myHashMap.set('Elizabeth', 'Something');
+// myHashMap.set('William', 'Something');
+// myHashMap.set('Richard', 'Something');
+myHashMap.set('Sarah', '3');
+
+console.log(myHashMap.buckets);
+
+// myHashMap.set('Mark', 'Something else');
+
+// console.log(myHashMap.buckets);
