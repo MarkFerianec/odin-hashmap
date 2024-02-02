cceefb759901a58f7354f92783fa72d8b3e66483
Mark Ferianec <134004771+MarkFerianec@users.noreply.github.com>
2024-02-02 14:11:19 -0700

    Modify hashmap
    Modified set function so that it overwrites values when the same key
    is entered.

--- hashmap.js
+++ hashmap.js
@@ -17,21 +17,45 @@ class HashMap {
   }
 
   set(key, value) {
-    // if the buckets have reached loadfactor of 0.75
-    // increase buckets size * 2, copy over all nodes.
-    // not just that, we need to make a linked list with
-    // new nodes added to shared buckets
-
     if (this.buckets[this.hash(key)] === undefined) {
       let newNode = new Node(key, value);
       this.buckets[this.hash(key)] = newNode;
-    } else {
-      let newNode = new Node(key, value);
+    }
+
+    if (this.buckets[this.hash(key)] !== undefined) {
+      // The beginning of linked list
       let temporary = this.buckets[this.hash(key)];
-      while (temporary.nextNode != null) {
+
+      let keyExists = false;
+
+      // if first node's key is same as key
+      if (temporary.key === key) {
+        temporary.value = value;
+        keyExists = true;
+      }
+
+      // I think this works for all nodes inbetween first and last?
+      while (temporary.nextNode !== null) {
+        if (temporary.key === key) {
+          temporary.value = value;
+          keyExists = true;
+          break;
+        }
         temporary = temporary.nextNode;
       }
-      temporary.nextNode = newNode;
+
+      // if last node's key is same as key
+      // Maybe I don't need to put temporary.nextNode === null in if statement?
+      // if (temporary.nextNode === null && temporary.key === key) {
+      if (temporary.key === key) {
+        temporary.value = value;
+        keyExists = true;
+      }
+
+      if (keyExists !== true) {
+        let newNode = new Node(key, value);
+        temporary.nextNode = newNode;
+      }
     }
   }
 }
@@ -52,30 +76,33 @@ let myHashMap = new HashMap();
 
 // console.log(myHashMap.hash('Mark'));
 // console.log(myHashMap.hash('Bob'));
-console.log(myHashMap.hash('Steven'));
+// console.log(myHashMap.hash('Steven')); // Hashes to 11
 // console.log(myHashMap.hash('James'));
 // console.log(myHashMap.hash('Patricia'));
 // console.log(myHashMap.hash('Jennifer'));
-console.log(myHashMap.hash('John'));
+// console.log(myHashMap.hash('John')); // Hashes to 11
 // console.log(myHashMap.hash('Elizabeth'));
 // console.log(myHashMap.hash('William'));
 // console.log(myHashMap.hash('Richard'));
-console.log(myHashMap.hash('Sarah'));
+// console.log(myHashMap.hash('Sarah')); // Hashes to 11
 
 // myHashMap.set('Mark', 'Something');
 // myHashMap.set('Bob', 'Something');
 myHashMap.set('Steven', '1');
+myHashMap.set('Steven', 'overwritten');
 // myHashMap.set('James', 'Something');
 // myHashMap.set('Patricia', 'Something');
 // myHashMap.set('Jennifer', 'Something');
 myHashMap.set('John', '2');
+myHashMap.set('John', 'overwrite John');
 // myHashMap.set('Elizabeth', 'Something');
 // myHashMap.set('William', 'Something');
 // myHashMap.set('Richard', 'Something');
 myHashMap.set('Sarah', '3');
+myHashMap.set('Sarah', 'overwrite Sarah');
+myHashMap.set('Sarah', 'overwrite Sarah AGAIN');
+myHashMap.set('Steven', 'Overwrite Steven later on');
+myHashMap.set('Sarah', 'Overwrite Sarah sometime later');
+myHashMap.set('John', 'Im done with this maybe');
 
 console.log(myHashMap.buckets);
-
-// myHashMap.set('Mark', 'Something else');
-
-// console.log(myHashMap.buckets);
